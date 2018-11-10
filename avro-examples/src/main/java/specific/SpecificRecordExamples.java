382e454101a8f6eaa62679a1e7b2b822f9afad03
Xuhui Wang <xuhuiw@andrew.cmu.edu>
2018-11-10 13:42:53 -0800

    added examples

--- avro-examples/src/main/java/specific/SpecificRecordExamples.java
+++ avro-examples/src/main/java/specific/SpecificRecordExamples.java
@@ -1,6 +1,15 @@
 package specific;
 
 import com.example.Customer;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SpecificRecordExamples {
     public static void main(String[] args) {
@@ -16,8 +25,33 @@ public class SpecificRecordExamples {
         System.out.println(customer);
 
         // step 2: write to file
+        final DatumWriter<Customer> datumWriter = new SpecificDatumWriter<>(Customer.class);
+        try (DataFileWriter<Customer> dataFileWriter = new DataFileWriter<>(datumWriter)) {
+            dataFileWriter.create(customer.getSchema(), new File("customer-specific.avro"));
+            dataFileWriter.append(customer);
+            System.out.println("successfully wrote customer-specific.avro");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
         // step 3: read from file
+        final File file = new File("customer-specific.avro");
+        final DatumReader<Customer> datumReader = new SpecificDatumReader<>(Customer.class);
+        final DataFileReader<Customer> dataFileReader;
+        try {
+            System.out.println("Reading our specific record");
+            dataFileReader = new DataFileReader<>(file, datumReader);
+            while (dataFileReader.hasNext()) {
+                Customer readCustomer = dataFileReader.next();
+                System.out.println(readCustomer.toString());
+                System.out.println("First name: " + readCustomer.getFirstName());
+
+                Float height = readCustomer.getHeight();
+                System.out.println(Float.toString(height));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // step 4: interpret
     }
